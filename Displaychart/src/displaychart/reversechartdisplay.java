a134844bb5adcb4360b0e8a652c6a9196faae36e
Milind Hase <Milind Hase@DESKTOP-4HCB293>
2022-07-03 10:16:31 +0530

    Display chart project program commited..... Check and  Approve task

---
+++ Displaychart/src/displaychart/reversechartdisplay.java
@@ -0,0 +1,25 @@
+package displaychart;
+
+public class reversechartdisplay {
+	public static void main(String[] args) {
+		
+		int i,j,n=20;
+		System.out.println("Display Multiplication Chart");
+		
+		for (i=20;i>=1;i--)
+		{
+			
+			for (j=20;j>=1;j--)
+			{	
+				
+			
+			System.out.print  (  i  *  j );
+			System.out.print("  "); 
+	 }
+			System.out.println("  ");
+			System.out.println("  ");
+			
+	 }
+	}
+
+}
